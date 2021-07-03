bda256039b0d0fb09e83c91236efb91079985450
Ashleigh Kasha <akasha@va-b5.kzn.21-school.ru>
2021-07-03 19:34:06 +0300

    add all overloads prototypes

--- src/Iterator/VectorIterator.hpp
+++ src/Iterator/VectorIterator.hpp
@@ -1,21 +1,56 @@
 #ifndef ITERATOR_HPP
 # define ITERATOR_HPP
 
+# include <iterator>
+
+
 namespace ft {
 
 	template <typename T>
 	class VectorIterator : public std::iterator<std::random_access_iterator_tag, T> {
 		private:
-			T* _ptr;
+			typedef	T			value_type;
+			typedef T*			pointer;
+			typedef T&			reference;
+			typedef ptrdiff_t	difference_type;
+
+			pointer _ptr;
 
 		public:
 			VectorIterator(T* ptr);
   			VectorIterator(const VectorIterator& other);
 
 			~VectorIterator();
-	};
 
-	# include "VectorIterator.inl"
+			VectorIterator& 	operator=(const VectorIterator& other);
+
+			bool 				operator==(const VectorIterator& other) const;
+        	bool 				operator!=(const VectorIterator& other) const;
+
+			reference			operator*();
+			pointer				operator->();
+
+			VectorIterator& 	operator++();
+			VectorIterator& 	operator--();
+			VectorIterator 		operator++(int);
+			VectorIterator 		operator--(int);
+
+        	bool 				operator>(const VectorIterator& other) const;
+        	bool 				operator>=(const VectorIterator& other) const;
+        	bool 				operator<(const VectorIterator& other) const;
+        	bool 				operator<=(const VectorIterator& other) const;
+
+			VectorIterator 		operator-=(int value);
+			VectorIterator 		operator+=(int value);
+			
+			VectorIterator 		operator-(int value) const;
+			VectorIterator 		operator+(int value) const;
+			difference_type 	operator-(const VectorIterator& other) const;
+
+			reference			operator[](int value) const;
+	};
 }
+
+# include "VectorIterator.inl"
 
 #endif
--- src/Iterator/VectorIterator.inl
+++ src/Iterator/VectorIterator.inl
@@ -1,16 +1,18 @@
-/*
-** Constructors
-*/
+namespace ft {
+	/*
+	** Constructors
+	*/
 
-template <typename T>
-VectorIterator<T>::VectorIterator(T* ptr) : _ptr(ptr) {};
+	template <typename T>
+	VectorIterator<T>::VectorIterator(T* ptr) : _ptr(ptr) {};
 
-template <typename T>
-VectorIterator<T>::VectorIterator(const VectorIterator& other) : _ptr(other._ptr) {};
+	template <typename T>
+	VectorIterator<T>::VectorIterator(const VectorIterator& other) : _ptr(other._ptr) {};
 
-/*
-** Destructors
-*/
+	/*
+	** Destructors
+	*/
 
-template <typename T>
-VectorIterator<T>::~VectorIterator() {};
+	template <typename T>
+	VectorIterator<T>::~VectorIterator() {};
+}

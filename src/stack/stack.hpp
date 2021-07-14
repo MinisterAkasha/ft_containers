27669f63769bfa7c32e69879b19edd489507d012
Ashleigh Kasha <gusevworkmail@gmail.com>
2021-07-14 23:12:27 +0300

    add initial stack struct

---
+++ src/stack/stack.hpp
@@ -0,0 +1,53 @@
+#ifndef STACK_HPP
+# define STACK_HPP
+
+# include "utils.hpp"
+# include "vector.hpp"
+# include <memory>
+
+namespace ft {
+
+	template <class T, class Container = ft::vector<T> >
+	class stack {
+		public:
+			typedef T			value_type;
+			typedef Container	container_type;
+			typedef size_t		size_type;
+
+		explicit stack(const container_type& ctnr = container_type());
+
+		/*
+		** Member functions
+		*/
+		bool 					empty() const;
+		size_type 				size() const;
+		value_type& 			top();
+		const value_type& 		top() const;
+		void 					push (const value_type& val);
+		void 					pop();
+	}
+
+	/*
+	** Non-member functions
+	*/
+
+	template <class T, class Container>
+	bool operator== (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+
+	template <class T, class Container>
+	bool operator!= (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+
+	template <class T, class Container>
+	bool operator<  (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+
+	template <class T, class Container>
+	bool operator<= (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+
+	template <class T, class Container>
+	bool operator>  (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+
+	template <class T, class Container>
+	bool operator>= (const stack<T,Container>& lhs, const stack<T,Container>& rhs);
+}
+
+#endif

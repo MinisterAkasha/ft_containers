a621b229699eba6ac086bb7b0be8a8dbdaf19b22
Ashleigh Kasha <gusevworkmail@gmail.com>
2021-07-14 21:17:24 +0300

    add non-member operator<

--- src/Iterator/reverseIterator.hpp
+++ src/Iterator/reverseIterator.hpp
@@ -110,7 +110,10 @@ namespace ft {
 	};
 
 	template <class Iterator>
-	bool operator<(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs);
+	bool operator<(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs) {
+		return lhs.base() > rhs.base();
+	};
+
 	template <class Iterator>
 	bool operator<=(const reverse_iterator<Iterator>& lhs, const reverse_iterator<Iterator>& rhs);
 	template <class Iterator>

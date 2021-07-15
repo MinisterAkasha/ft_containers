fa70f1d58d78ed724128c09c1dddfe2ac68698f3
Ashleigh Kasha <gusevworkmail@gmail.com>
2021-07-15 13:57:34 +0300

    add push stack tests

--- Tests/StackTest/StackTest.cpp
+++ Tests/StackTest/StackTest.cpp
@@ -88,6 +88,105 @@ void	StackTest::testConstructor() const {
 
 void	StackTest::testPush() const {
 	std::cout << "* push: ";
+	{
+		ft::stack<int, ft::vector<int> > ftS;
+		std::stack<int, std::vector<int> > stlS;
+
+		ftS.push(10);
+		stlS.push(10);
+
+		PRINT_RESULT(TO_EQUAL_STACK(ftS, stlS));
+	}
+	{
+		ft::stack<int, ft::vector<int> > ftS;
+		std::stack<int, std::vector<int> > stlS;
+
+		for (int i = 0; i < 10; i++) {
+			ftS.push(i);
+			stlS.push(i);
+		}
+
+		PRINT_RESULT(TO_EQUAL_STACK(ftS, stlS));
+	}
+	{
+		ft::stack<int, ft::vector<int> > ftS;
+		std::stack<int, std::vector<int> > stlS;
+
+		createRandomStack(ftS, stlS, 100);
+
+		PRINT_RESULT(TO_EQUAL_STACK(ftS, stlS));
+	}
+	{
+		ft::vector<int> ftV(10, 100);
+		std::vector<int> stlV(10, 100);
+
+		ft::stack<int, ft::vector<int> > ftS(ftV);
+		std::stack<int, std::vector<int> > stlS(stlV);
+
+		ftS.push(0);
+		stlS.push(0);
+
+		PRINT_RESULT(TO_EQUAL_STACK(ftS, stlS));
+	}
+	{
+		ft::vector<int> ftV;
+		std::vector<int> stlV;
+
+		ft::stack<int, ft::vector<int> > ftS(ftV);
+		std::stack<int, std::vector<int> > stlS(stlV);
+
+		createRandomStack(ftS, stlS, 100);
+
+		PRINT_RESULT(TO_EQUAL_STACK(ftS, stlS));
+	}
+	{
+		ft::vector<int> ftV(10, 100);
+		std::vector<int> stlV(10, 100);
+
+		ft::stack<int, ft::vector<int> > ftS(ftV);
+		std::stack<int, std::vector<int> > stlS(stlV);
+
+		createRandomStack(ftS, stlS, 100);
+
+		PRINT_RESULT(TO_EQUAL_STACK(ftS, stlS));
+	}
+	{
+		std::list<int> list1;
+		std::list<int> list2;
+
+		createRandomList(list1, list2, 100);
+
+		ft::stack<int, std::list<int> > ftS(list1);
+		std::stack<int, std::list<int> > stlS(list2);
+
+		createRandomStack(ftS, stlS, 5);
+
+		PRINT_RESULT(TO_EQUAL_STACK(ftS, stlS));
+	}
+	{
+		std::list<int> list1;
+		std::list<int> list2;
+
+		ft::stack<int, std::list<int> > ftS(list1);
+		std::stack<int, std::list<int> > stlS(list2);
+
+		createRandomStack(ftS, stlS, 5);
+
+		PRINT_RESULT(TO_EQUAL_STACK(ftS, stlS));
+	}
+	{
+		std::list<int> list1;
+		std::list<int> list2;
+
+		createRandomList(list1, list2, 100);
+
+		ft::stack<int, std::list<int> > ftS(list1);
+		std::stack<int, std::list<int> > stlS(list2);
+
+		createRandomStack(ftS, stlS, 15);
+
+		PRINT_RESULT(TO_EQUAL_STACK(ftS, stlS));
+	}
 	std::cout << std::endl;
 }
 

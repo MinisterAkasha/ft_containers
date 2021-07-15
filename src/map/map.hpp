eff1d8834289ae458001a5f7347588ccc1911961
Ashleigh Kasha <gusevworkmail@gmail.com>
2021-07-15 16:13:05 +0300

    add map initial structure

--- src/map/map.hpp
+++ src/map/map.hpp
@@ -33,6 +33,96 @@ namespace ft {
 
 			class value_compare;
 
+			/*
+			** Constructor
+			*/
+
+			explicit map (const key_compare& comp = key_compare(),
+						const allocator_type& alloc = allocator_type());	// empty
+
+			template <class InputIterator>
+			map (InputIterator first, InputIterator last,
+				const key_compare& comp = key_compare(),
+				const allocator_type& alloc = allocator_type());			// range
+
+			map (const map& x);												// copy
+
+			map& operator= (const map& x);
+
+			/*
+			** Destructor
+			*/
+
+			~map();
+
+			/*
+			** Iterators
+			*/
+
+			iterator				begin();
+			const_iterator			begin() const;
+			iterator 				end();
+			const_iterator 			end() const;
+			reverse_iterator		rbegin();
+			const_reverse_iterator	rbegin() const;
+			reverse_iterator		rend();
+			const_reverse_iterator	rend() const;
+
+			/*
+			** Iterators
+			*/
+
+			bool 					empty() const;
+			size_type				size() const;
+			size_type				max_size() const;
+
+			/*
+			** Element access
+			*/
+
+			mapped_type& 			operator[](const key_type& k);
+
+			/*
+			** Modifiers
+			*/
+
+			pair<iterator,bool> 	insert(const value_type& val);						// single element
+			iterator 				insert(iterator position, const value_type& val);	// with hint
+			template <class InputIterator>
+			void 					insert(InputIterator first, InputIterator last);	// range
+			void 					erase(iterator position);
+			size_type 				erase(const key_type& k);
+			void 					erase(iterator first, iterator last);
+			void 					swap(map& x);
+			void					clear();
+
+			/*
+			** Observers
+			*/
+
+			key_compare				key_comp() const;
+			value_compare 			value_comp() const;
+
+			/*
+			** Operations
+			*/
+
+			iterator 				find(const key_type& k);
+			const_iterator			find(const key_type& k) const;
+			size_type 				count(const key_type& k) const;
+			iterator				lower_bound(const key_type& k);
+			const_iterator			lower_bound(const key_type& k) const;
+			iterator 				upper_bound(const key_type& k);
+			const_iterator			upper_bound(const key_type& k) const;
+			pair<const_iterator,
+			const_iterator>			equal_range (const key_type& k) const;
+			pair<iterator,iterator> equal_range (const key_type& k);
+
+			/*
+			** Allocator
+			*/
+
+			allocator_type 			get_allocator() const;
 
 		private:
 	};

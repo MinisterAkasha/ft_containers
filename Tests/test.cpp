9b4e0896c28eca87a9176261acc20f7940f9ebce
Ashleigh Kasha <gusevworkmail@gmail.com>
2021-07-24 23:36:56 +0300

    add final tests

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -5,61 +5,9 @@
 #include "map.hpp"
 
 int main() {
-	// ft::map<int, int>			ftMap;
-	
-
-	// while (end != begin) {
-	// 	end--;
-	// 	std::cout << end->first << " ";
-	// }
-	// std::cout  << std::endl;
-
-	// for (int i = 0; i < 12; i++) {
-	// 	end--;
-	// 	std::cout << end->first << std::endl;
-	// }
-	// for (int i = 0; i < 10; i++) {
-	// 	if (i % 2)
-	// 		ftMap.insert(ft::make_pair(-i, -i));
-	// 	else
-	// 		ftMap.insert(ft::make_pair(i, i));
-	// }
-
-	// ft::map<int, int>::iterator begin = ftMap.begin();
-	// ft::map<int, int>::iterator end = ftMap.end();
-
-	// while (end != begin) {
-	// 	std::cout << begin->first << " ";
-	// 	begin++;
-	// }
-	// std::cout  << std::endl;
-
-	// for (int i = 0; i < 10; i++) {
-	// 	std::cout << begin->first << std::endl;
-	// 	begin++;
-	// }
-
-	// std::cout << itr->first << std::endl;
-	// ++itr;
-	// std::cout << itr->first << std::endl;
-	// ++itr;
-	// std::cout << itr->first << std::endl;
-	// ++itr;
-	// std::cout << itr->first << std::endl;
-	// ++itr;
-	// std::cout << itr->first << std::endl;
-	// ++itr;
-	// std::cout << itr->first << std::endl;
-	// ++itr;
-	// std::cout << itr->first << std::endl;
-	// ++itr;
-	// std::cout << itr->first << std::endl;
-	// ++itr;
-	// std::cout << itr->first << std::endl;
-
-	// VectorTest 				vectorTest;
-	// StackTest				stackTest;
-	// ReverseIteratorTest 	reverseIteratorTest;
+	VectorTest 				vectorTest;
+	StackTest				stackTest;
+	ReverseIteratorTest 	reverseIteratorTest;
 	MapTest					mapTest;
 	return 0;
 }
